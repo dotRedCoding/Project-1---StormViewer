4cd617438bd75b78082e5ce5f5f49a03c8afc1eb
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-04-21 15:45:50 -0400

    - basic project set up - imported contents folder containing a set of images - basic loop to go through pointed file directory to look for the images with a specific prefix and add them to an array

--- Project 1 - StormViewer/ViewController.swift
+++ Project 1 - StormViewer/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - PROPERTY
+    var pictures = [String]()
+    
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        let fm = FileManager.default // built in system type for the File System
+        let path = Bundle.main.resourcePath! // Bundle is a directory of our compiled program and all our assets
+        let items = try! fm.contentsOfDirectory(atPath: path) // set the contents of the new constant to the pointed path (the one above where the assests are)
+        
+        for item in items { // loop through all the items found in the directory
+            if item.hasPrefix("nssl") {
+                // this is the picture to load
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    } // End of viewDidLoad
 
+    
+    
+    
+    
+    
 
-}
+} // End of ViewController
 
